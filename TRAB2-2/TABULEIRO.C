6ab7bdae7c6d8f73c43e117f5468802c62860699
azevedog <gustavobhazevedo@gmail.com>
2016-10-04 23:52:32 -0300

    Continuacao do modulo tabuleiro

--- TRAB2-2/TABULEIRO.C
+++ TRAB2-2/TABULEIRO.C
@@ -76,395 +76,114 @@
 
 /***************************************************************************
 *
-*  Função: LIS  &Criar lista
+*  Função: TAB  &Criar Tabuleiro
 *  ****/
+TAB_tpCondRet TAB_CriarTabuleiro(int numColunas, int numLinhas,
+		TAB_tppTabuleiro* novoTabuleiro){
+		
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB  &Criar Tabuleirol */
 
-   LIS_tppLista LIS_CriarLista(
-             void   ( * ExcluirValor ) ( void * pDado ) )
-   {
-
-      LIS_tpLista * pLista = NULL ;
-
-      pLista = ( LIS_tpLista * ) malloc( sizeof( LIS_tpLista )) ;
-      if ( pLista == NULL )
-      {
-         return NULL ;
-      } /* if */
-
-      LimparCabeca( pLista ) ;
-
-      pLista->ExcluirValor = ExcluirValor ;
-
-      return pLista ;
-
-   } /* Fim função: LIS  &Criar lista */
 
 /***************************************************************************
 *
-*  Função: LIS  &Destruir lista
+*  Função: TAB  &Inserir peca no tabuleiro
 *  ****/
+ TAB_tpCondRet TAB_InserirPeca(int linha, char coluna,
+		void* peca){
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB  &Inserir peca no tabuleiro */
 
-   void LIS_DestruirLista( LIS_tppLista pLista )
-   {
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      LIS_EsvaziarLista( pLista ) ;
-
-      free( pLista ) ;
-
-   } /* Fim função: LIS  &Destruir lista */
+  
 
 /***************************************************************************
 *
-*  Função: LIS  &Esvaziar lista
+*  Função: TAB  &Mover peca no tabuleiro
 *  ****/
+ TAB_tpCondRet TAB_MoverPeca(int inicialX, char inicialY, int finalX, char finalY){
+		
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB  &Mover peca no tabuleiro */
 
-   void LIS_EsvaziarLista( LIS_tppLista pLista )
-   {
-
-      tpElemLista * pElem ;
-      tpElemLista * pProx ;
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      pElem = pLista->pOrigemLista ;
-      while ( pElem != NULL )
-      {
-         pProx = pElem->pProx ;
-         LiberarElemento( pLista , pElem ) ;
-         pElem = pProx ;
-      } /* while */
-
-      LimparCabeca( pLista ) ;
-
-   } /* Fim função: LIS  &Esvaziar lista */
+   
+/***************************************************************************
+*
+*  Função: TAB  &Retirar peca do tabuleiro
+*  ****/
+ TAB_tpCondRet TAB_RetirarPeca(int inicialX, char inicialY){
+		
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB  &Retirar peca do tabuleiro */
+   
 
 /***************************************************************************
 *
-*  Função: LIS  &Inserir elemento antes
+*  Função: TAB  &Obter peca do tabuleiro
 *  ****/
-
-   LIS_tpCondRet LIS_InserirElementoAntes( LIS_tppLista pLista ,
-                                           void * pValor        )
-   {
-
-      tpElemLista * pElem ;
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      /* Criar elemento a inerir antes */
-
-         pElem = CriarElemento( pLista , pValor ) ;
-         if ( pElem == NULL )
-         {
-            return LIS_CondRetFaltouMemoria ;
-         } /* if */
-
-      /* Encadear o elemento antes do elemento corrente */
-
-         if ( pLista->pElemCorr == NULL )
-         {
-            pLista->pOrigemLista = pElem ;
-            pLista->pFimLista = pElem ;
-         } else
-         {
-            if ( pLista->pElemCorr->pAnt != NULL )
-            {
-               pElem->pAnt  = pLista->pElemCorr->pAnt ;
-               pLista->pElemCorr->pAnt->pProx = pElem ;
-            } else
-            {
-               pLista->pOrigemLista = pElem ;
-            } /* if */
-
-            pElem->pProx = pLista->pElemCorr ;
-            pLista->pElemCorr->pAnt = pElem ;
-         } /* if */
-
-         pLista->pElemCorr = pElem ;
-
-         return LIS_CondRetOK ;
-
-   } /* Fim função: LIS  &Inserir elemento antes */
+ TAB_tpCondRet TAB_ObterPeca(int inicialX, char inicialY, char* nome, char* cor){
+		
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB  &Obter peca do tabuleiro */
+	
 
 /***************************************************************************
 *
-*  Função: LIS  &Inserir elemento após
+*  Função: TAB  &Obter ameacantes
 *  ****/
+ TAB_tpCondRet TAB_ObterListaAmeacantes(int inicialX, char inicialY, LIS_tppLista* lista){
+		
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB  &Obter ameacantes */
 
-   LIS_tpCondRet LIS_InserirElementoApos( LIS_tppLista pLista ,
-                                          void * pValor        )
-      
-   {
-
-      tpElemLista * pElem ;
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      /* Criar elemento a inerir após */
-
-         pElem = CriarElemento( pLista , pValor ) ;
-         if ( pElem == NULL )
-         {
-            return LIS_CondRetFaltouMemoria ;
-         } /* if */
-
-      /* Encadear o elemento após o elemento */
-
-         if ( pLista->pElemCorr == NULL )
-         {
-            pLista->pOrigemLista = pElem ;
-            pLista->pFimLista = pElem ;
-         } else
-         {
-            if ( pLista->pElemCorr->pProx != NULL )
-            {
-               pElem->pProx  = pLista->pElemCorr->pProx ;
-               pLista->pElemCorr->pProx->pAnt = pElem ;
-            } else
-            {
-               pLista->pFimLista = pElem ;
-            } /* if */
-
-            pElem->pAnt = pLista->pElemCorr ;
-            pLista->pElemCorr->pProx = pElem ;
-
-         } /* if */
-                  
-         pLista->pElemCorr = pElem ;
-                  
-         return LIS_CondRetOK ;
-
-   } /* Fim função: LIS  &Inserir elemento após */
-
+	
 /***************************************************************************
 *
-*  Função: LIS  &Excluir elemento
+*  Função: TAB  &Obter ameacados
 *  ****/
+ TAB_tpCondRet TAB_ObterListaAmeacados(int inicialX, char inicialY, LIS_tppLista* lista){
+		
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB  &Obter ameacados */
 
-   LIS_tpCondRet LIS_ExcluirElemento( LIS_tppLista pLista )
-   {
-
-      tpElemLista * pElem ;
-
-      #ifdef _DEBUG
-         assert( pLista  != NULL ) ;
-      #endif
-
-      if ( pLista->pElemCorr == NULL )
-      {
-         return LIS_CondRetListaVazia ;
-      } /* if */
-
-      pElem = pLista->pElemCorr ;
-
-      /* Desencadeia à esquerda */
-
-         if ( pElem->pAnt != NULL )
-         {
-            pElem->pAnt->pProx   = pElem->pProx ;
-            pLista->pElemCorr    = pElem->pAnt ;
-         } else {
-            pLista->pElemCorr    = pElem->pProx ;
-            pLista->pOrigemLista = pLista->pElemCorr ;
-         } /* if */
-
-      /* Desencadeia à direita */
-
-         if ( pElem->pProx != NULL )
-         {
-            pElem->pProx->pAnt = pElem->pAnt ;
-         } else
-         {
-            pLista->pFimLista = pElem->pAnt ;
-         } /* if */
-
-      LiberarElemento( pLista , pElem ) ;
-
-      return LIS_CondRetOK ;
-
-   } /* Fim função: LIS  &Excluir elemento */
-
-/***************************************************************************
+/***********************************************************************
 *
-*  Função: LIS  &Obter referência para o valor contido no elemento
-*  ****/
-
-   void * LIS_ObterValor( LIS_tppLista pLista )
-   {
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      if ( pLista->pElemCorr == NULL )
-      {
-        return NULL ;
-      } /* if */
-
-      return pLista->pElemCorr->pValor ;
-
-   } /* Fim função: LIS  &Obter referência para o valor contido no elemento */
-
-/***************************************************************************
+*  $FC Função: TAB  &Destriuir tabuleiro
 *
-*  Função: LIS  &Ir para o elemento inicial
-*  ****/
-
-   void IrInicioLista( LIS_tppLista pLista )
-   {
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      pLista->pElemCorr = pLista->pOrigemLista ;
-
-   } /* Fim função: LIS  &Ir para o elemento inicial */
-
-/***************************************************************************
+*  $ED Descrição da função
+*     Libera a memoria do tabuleiro e todas as suas pecas.
 *
-*  Função: LIS  &Ir para o elemento final
-*  ****/
-
-   void IrFinalLista( LIS_tppLista pLista )
-   {
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      pLista->pElemCorr = pLista->pFimLista ;
-
-   } /* Fim função: LIS  &Ir para o elemento final */
-
-/***************************************************************************
+*  $EP Parâmetros
+*     inicialX  - Coordenada horizontal da posicao da peca
 *
-*  Função: LIS  &Avançar elemento
-*  ****/
-
-   LIS_tpCondRet LIS_AvancarElementoCorrente( LIS_tppLista pLista ,
-                                              int numElem )
-   {
-
-      int i ;
-
-      tpElemLista * pElem ;
-
-      #ifdef _DEBUG
-         assert( pLista != NULL ) ;
-      #endif
-
-      /* Tratar lista vazia */
-
-         if ( pLista->pElemCorr == NULL )
-         {
-
-            return LIS_CondRetListaVazia ;
-
-         } /* fim ativa: Tratar lista vazia */
-
-      /* Tratar avançar para frente */
-
-         if ( numElem > 0 )
-         {
-
-            pElem = pLista->pElemCorr ;
-            for( i = numElem ; i > 0 ; i-- )
-            {
-               if ( pElem == NULL )
-               {
-                  break ;
-               } /* if */
-               pElem    = pElem->pProx ;
-            } /* for */
-
-            if ( pElem != NULL )
-            {
-               pLista->pElemCorr = pElem ;
-               return LIS_CondRetOK ;
-            } /* if */
-
-            pLista->pElemCorr = pLista->pFimLista ;
-            return LIS_CondRetFimLista ;
-
-         } /* fim ativa: Tratar avançar para frente */
-
-      /* Tratar avançar para trás */
-
-         else if ( numElem < 0 )
-         {
-
-            pElem = pLista->pElemCorr ;
-            for( i = numElem ; i < 0 ; i++ )
-            {
-               if ( pElem == NULL )
-               {
-                  break ;
-               } /* if */
-               pElem    = pElem->pAnt ;
-            } /* for */
-
-            if ( pElem != NULL )
-            {
-               pLista->pElemCorr = pElem ;
-               return LIS_CondRetOK ;
-            } /* if */
-
-            pLista->pElemCorr = pLista->pOrigemLista ;
-            return LIS_CondRetFimLista ;
-
-         } /* fim ativa: Tratar avançar para trás */
-
-      /* Tratar não avançar */
-
-         return LIS_CondRetOK ;
-
-   } /* Fim função: LIS  &Avançar elemento */
-
-/***************************************************************************
+*     inicialY  - Coordenada vertical da posicao  da peca
 *
-*  Função: LIS  &Procurar elemento contendo valor
+*     lista  - Ponteirto para o cabecalho da lista (retornado por referencia).
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - Se executou corretamente.
+*     TAB_CondRetErro - Caso contrário.
+* 
+*
+***********************************************************************/
+
+	/***************************************************************************
+*
+*  Função: TAB  &Destriuir tabuleiro
 *  ****/
+ TAB_tpCondRet TAB_DestruirTabuleiro(){
+		
+		
+		return TAB_CondRetOK;
+	}/* Fim função: TAB &Destriuir tabuleiro */
+	
 
-   LIS_tpCondRet LIS_ProcurarValor( LIS_tppLista pLista ,
-                                    void * pValor        )
-   {
-
-      tpElemLista * pElem ;
-
-      #ifdef _DEBUG
-         assert( pLista  != NULL ) ;
-      #endif
-
-      if ( pLista->pElemCorr == NULL )
-      {
-         return LIS_CondRetListaVazia ;
-      } /* if */
-
-      for ( pElem  = pLista->pElemCorr ;
-            pElem != NULL ;
-            pElem  = pElem->pProx )
-      {
-         if ( pElem->pValor == pValor )
-         {
-            pLista->pElemCorr = pElem ;
-            return LIS_CondRetOK ;
-         } /* if */
-      } /* for */
-
-      return LIS_CondRetNaoAchou ;
-
-   } /* Fim função: LIS  &Procurar elemento contendo valor */
 
 
 /*****  Código das funções encapsuladas no módulo  *****/
